64191fc291ac6d5fdeb4501d4be2eb71d000e056
Hugo Poujol <hugo.poujol@epita.fr>
2022-06-27 01:23:00 +0200

    feat: Dungeon Map with random generation + Victory / Defeat screens

--- Assets/Scripts/DungeonMap.cs
+++ Assets/Scripts/DungeonMap.cs
@@ -8,21 +8,14 @@ using UnityEngine.UI;
 public class DungeonMap : MonoBehaviour
 {
     [SerializeField]
-    private Button[] buttons;
-    [SerializeField]
-    private Canvas ResultScreen;
+    private FlexibleGridLayout flexibleGrid;
 
     private void Start()
     {
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            if (i != DungeonManager.instance.currentFloor)
-            {
-                buttons[i].interactable = false;
-            }
-        }
-
         bool alive = false;
+        bool? victory = null;
+
+
         foreach (var character in DungeonManager.instance.characters)
         {
             if (character.currentHP > 0)
@@ -30,19 +23,58 @@ public class DungeonMap : MonoBehaviour
         }
         if (!alive)
         {
-            ResultScreen.GetComponent<CanvasGroup>().alpha = 1;
-            ResultScreen.GetComponentInChildren<TextMeshProUGUI>().text = "Défaite";
-            ResultScreen.GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+            flexibleGrid.createDefeatText();
+            StartCoroutine(flexibleGrid.AutoScroll(2f));
+            victory = false;
         }
-        else if (DungeonManager.instance.currentFloor == DungeonManager.maxFloor)
+        else if (DungeonManager.instance.currentFloor == DungeonManager.instance.maxFloor)
         {
-            ResultScreen.GetComponent<CanvasGroup>().alpha = 1;
-            ResultScreen.GetComponentInChildren<TextMeshProUGUI>().text = "Victoire";
-            ResultScreen.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
+            flexibleGrid.createVictoryText();
+            StartCoroutine(flexibleGrid.AutoScroll(2f));
+            victory = true;
         }
+        flexibleGrid.generateMap(victory);
     }
 
-    public void LoadLevel(int level)
+    public static List<List<Room.RoomType>> GenerateMap(int seed)
+    {
+        Random.InitState(seed);
+        int dungeonSize = Random.Range(4, 8);
+        List<List<Room.RoomType>> map = new List<List<Room.RoomType>>(dungeonSize);
+        map.Add(new List<Room.RoomType>() { Room.RoomType.Start });
+        for (int i = 0; i < dungeonSize - 1; i++)
+        {
+            int rowSize = Random.Range(1, 4);
+            List<Room.RoomType> row = new List<Room.RoomType>(rowSize);
+            for (int j = 0; j < rowSize; j++)
+            {
+                var r = Random.Range(0, 10);
+                switch (r)
+                {
+                    case < 2:
+                        row.Add(Room.RoomType.Heal);
+                        break;
+
+                    case < 4:
+                        row.Add(Room.RoomType.Random);
+                        break;
+
+                    case < 6:
+                        row.Add(Room.RoomType.Bonus);
+                        break;
+
+                    default:
+                        row.Add(Room.RoomType.Fight);
+                        break;
+                }
+            }
+            map.Add(row);
+        }
+        map.Add(new List<Room.RoomType>() { Room.RoomType.Boss });
+        return map;
+    }
+
+    public static void LoadLevel(int level)
     {
         DungeonManager.DungeonMonsterInfo[] enemies = null;
         switch (level)
@@ -63,6 +95,8 @@ public class DungeonMap : MonoBehaviour
                 enemies[2] = new DungeonManager.DungeonMonsterInfo("Slime", 50);
                 break;
             default:
+                enemies = new DungeonManager.DungeonMonsterInfo[1];
+                enemies[0] = new DungeonManager.DungeonMonsterInfo("Slime", 50);
                 break;
         }
 
