4d2f39e5c5eeabb3bd4d8df9fb27d078bbc3f99a
eliott <eliott.bru@gmail.com>
2022-07-04 14:34:34 +0100

    feat: Add different dungeons

--- Assets/Scripts/Map/DungeonPortal.cs
+++ Assets/Scripts/Map/DungeonPortal.cs
@@ -6,6 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class DungeonPortal : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mesh;
+    [SerializeField]
+    private Material originMaterial;
+
+    private long id = -1;
+
+    private void Start()
+    {
+        id = long.Parse(transform.name);
+        Material material = new Material(originMaterial);
+        switch (id % 4)
+        {
+            case 0:
+                material.color = Color.red;
+                break;
+            case 1:
+                material.color = Color.grey;
+                break;
+            case 2:
+                material.color = Color.blue;
+                break;
+            case 3:
+                material.color = Color.green;
+                break;
+        }
+        mesh.GetComponent<MeshRenderer>().material = material;
+    }
+
     public bool EnterDungeon()
     {
         GameObject player = GameObject.Find("LocationBasedGame/Character");
@@ -28,6 +57,22 @@ public class DungeonPortal : MonoBehaviour
         description.GetComponent<CanvasGroup>().blocksRaycasts = true;
         description.GetComponent<CanvasGroup>().alpha = 1;
         description.GetComponent<DungeonDescription>().dungeon = gameObject;
-        description.transform.Find("Background/Title").GetComponent<TextMeshProUGUI>().text = transform.name;
+        string title = "";
+        switch (id % 4)
+        {
+            case 0:
+                title = "Antre de Daarun";
+                break;
+            case 1:
+                title = "La tour ténébreuse";
+                break;
+            case 2:
+                title = "Le donjon des oubliés";
+                break;
+            case 3:
+                title = "La forêt ensanglantée";
+                break;
+        }
+        description.transform.Find("Background/Title").GetComponent<TextMeshProUGUI>().text = title;
     }
 }
