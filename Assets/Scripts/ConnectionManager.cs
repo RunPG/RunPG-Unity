5d78d8f2b5d51d68778f9bbd4df5ea9ff0cc3c56
eliott <eliott.bru@gmail.com>
2022-07-06 14:23:27 +0100

    feat: Add player profile

--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -22,15 +22,11 @@ public class ConnectionManager : MonoBehaviour
     {
         if (status == SignInStatus.Success)
         {
-            // Continue with Play Games Services
             Social.ReportProgress(GPGSIds.achievement_bienvenue__kheg, 100.0f, null);
-            PlayGamesPlatform.Instance.RequestServerSideAccess(false, code => 
-            {
-                Debug.Log("code: " + code);
-                Debug.Log("User id: " + Social.localUser.id);
-                Debug.Log("User name: " + Social.localUser.userName);
-                SceneManager.LoadScene("DungeonScene");
-            });
+
+            PlayerProfile.pseudo = Social.localUser.userName;
+
+            SceneManager.LoadScene("MapScene");
         }
         else
         {
