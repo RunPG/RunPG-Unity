85a3b82cdd6d17488ba856fe0dfdef923c68c993
eliott <eliott.bru@gmail.com>
2022-07-06 11:56:13 +0100

    fix: input blocked when description is showing

--- Assets/Scripts/Map/PlayerMovement.cs
+++ Assets/Scripts/Map/PlayerMovement.cs
@@ -31,6 +31,9 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField]
 	private Animator animator;
 
+	[SerializeField]
+	private CanvasGroup dungeonDescription;
+
 	/// <summary>
 	/// Use a mock <see cref="T:Mapbox.Unity.Location.TransformLocationProvider"/>,
 	/// rather than a <see cref="T:Mapbox.Unity.Location.EditorLocationProvider"/>. 
@@ -106,31 +109,33 @@ public class PlayerMovement : MonoBehaviour
 		transform.localPosition = Vector3.Lerp(transform.localPosition, _targetPosition, Time.deltaTime * _positionFollowFactor);
 
 		animator.SetFloat("Speed", Vector2.Distance(transform.position, _targetPosition) / 200f);
-
-		foreach (var touch in Input.touches)
-        {
-			if (touch.phase == TouchPhase.Began)
-            {
-				touchDidMove[touch.fingerId] = false;
-            }
-			else if (touch.phase == TouchPhase.Moved)
-            {
-				touchDidMove[touch.fingerId] = true;
-				CameraPivot.transform.Rotate(0, 0.05f * touch.deltaPosition.x, 0);
+		if (dungeonDescription.alpha == 0)
+		{
+			foreach (var touch in Input.touches)
+			{
+				if (touch.phase == TouchPhase.Began)
+				{
+					touchDidMove[touch.fingerId] = false;
+				}
+				else if (touch.phase == TouchPhase.Moved)
+				{
+					touchDidMove[touch.fingerId] = true;
+					CameraPivot.transform.Rotate(0, 0.05f * touch.deltaPosition.x, 0);
+				}
+				else if (touch.phase == TouchPhase.Ended && !touchDidMove[touch.fingerId])
+				{
+					Ray ray = Camera.main.ScreenPointToRay(touch.position);
+					RaycastHit hit;
+					if (Physics.Raycast(ray, out hit))
+					{
+						DungeonPortal portal = hit.transform.gameObject.GetComponent<DungeonPortal>();
+						if (portal != null)
+						{
+							portal.ShowInfo();
+						}
+					}
+				}
 			}
-			else if (touch.phase == TouchPhase.Ended && !touchDidMove[touch.fingerId])
-            {
-				Ray ray = Camera.main.ScreenPointToRay(touch.position);
-				RaycastHit hit;
-				if (Physics.Raycast(ray, out hit))
-                {
-					DungeonPortal portal = hit.transform.gameObject.GetComponent<DungeonPortal>();
-					if (portal != null)
-                    {
-						portal.ShowInfo();
-                    }
-                }
-            }
-        }
+		}
 	}
 }
